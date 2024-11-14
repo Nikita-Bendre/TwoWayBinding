c4357a21d5b917e839adbf2cc7196f7baa19a183
Nikita Bendre <nickybendre898@gmail.com>
2024-11-14 21:59:20 +0530

    twowaybinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MarvellousComponent } from './marvellous/marvellous.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MarvellousComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
